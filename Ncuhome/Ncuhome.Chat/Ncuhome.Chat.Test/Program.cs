d3d96455163c181e8ffe1efbce117880ff308536
penjilu <penjilu@29880cb4-1cdc-11de-a78b-a7aca27b2395>
2010-11-13 15:14:33 +0000

    添加测试代码

--- Ncuhome/Ncuhome.Chat/Ncuhome.Chat.Test/Program.cs
+++ Ncuhome/Ncuhome.Chat/Ncuhome.Chat.Test/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Ncuhome.Chat.Test
 {
@@ -9,6 +10,26 @@ namespace Ncuhome.Chat.Test
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 1000; i++)
+            {
+                IISChatServerTest();
+            }
+
+            Console.Read();
+        }
+        static void IISChatServerTest()
+        {
+            AsyncWebRequest request = new AsyncWebRequest();
+            request.DoRequestAsync("http://localhost/chatclient.ashx",
+                (st) =>
+                {
+                    StreamReader sr = new StreamReader(st);
+                    Console.WriteLine(sr.ReadToEnd());
+                    sr.Close();
+                    st.Close();
+                    //一个连接结束，立即开始下一个
+                    IISChatServerTest();
+                });
         }
     }
 }
--- Ncuhome/Ncuhome.Chat/Ncuhome.Chat.WebSite/Default.aspx.cs
+++ Ncuhome/Ncuhome.Chat/Ncuhome.Chat.WebSite/Default.aspx.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using Ncuhome.Chat.IISChatServer;
+using System.Web.Query.Dynamic;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+       
     }
     static int msgId = 1;
     protected void Button1_Click(object sender, EventArgs e)

1a51218b9eadf429b99fa59244d13ea50b30a0d9
penjilu <penjilu@29880cb4-1cdc-11de-a78b-a7aca27b2395>
2010-11-28 11:15:16 +0000

    整理

--- trunk/Ncuhome/Ncuhome.Chat/Ncuhome.Chat.WebSite/Default.aspx.cs
+++ trunk/Ncuhome/Ncuhome.Chat/Ncuhome.Chat.WebSite/Default.aspx.cs
@@ -5,12 +5,31 @@ using System.Web;
 using System.Web.UI;
 using Ncuhome.Chat.SimpleThreadPool;
 using Ncuhome.Chat.Model;
+using System.Threading;
+using System.Web.Caching;
 
 public partial class _Default : System.Web.UI.Page
 {
+  
+
+    public  void Insert(string key, object obj, double Minutes)
+    {
+        Cache cache = new Cache();
+        if (obj != null)
+        {
+            this.Response.Write(cache);
+            cache.Remove("dd");
+            cache.Insert(key, obj, null, DateTime.Now.AddMinutes(Minutes), Cache.NoSlidingExpiration, CacheItemPriority.High, (a,b,c) => { });
+        }
+        
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
-       
+        
+        Insert("dd", "sss", 1);
+        
+     //  HttpWorkerRequest
     }
     static int msgId = 1;
     protected void Button1_Click(object sender, EventArgs e)
